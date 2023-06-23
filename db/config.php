3472937d71c8062422c2bd047395657ead1b452f
Matheus Almeida <105659915+mthalmeida@users.noreply.github.com>
2023-06-23 14:34:08 -0300

    refactor: Update config.php

--- db/config.php
+++ db/config.php
@@ -1,11 +1,11 @@
-<?php
-$Ser_Alt1 = "4285509FD5C67AFED068D51BACD396FA44EB624EC9EE9F2A56";
+ <?php
+$Ser_Alt1 = "SUA_SENHA_INICIAL";
 $Ser_Alt2 = hash('sha256', $Ser_Alt1);
 $Ser_Alt3 = hash('sha256', $Ser_Alt2);
 $Ser_Alt4 = hash('sha256', $Ser_Alt3);
 $Ser_Alt5 = hash('sha1', $Ser_Alt4);
 
-define("SERVIDOR", "mysql670.umbler.com:41890");
-define("USUARIO", "mthalmeida");
+define("SERVIDOR", "SEU_SERVIDOR_COM_PORTA");
+define("USUARIO", "USUARIO");
 define("SENHA", $Ser_Alt5);
-define("BANCO", "taskmananger");
+define("BANCO", "NOME_DO_BANCO");
